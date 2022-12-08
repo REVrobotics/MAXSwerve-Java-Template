196f9d2ce712cfae632a76750f74999d2be78845
jfabellera <jfabellera@gmail.com>
2022-12-08 16:09:32 -0600

    Refactor constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -25,6 +25,12 @@ import edu.wpi.first.math.util.Units;
  */
 public final class Constants {
   public static final class DriveConstants {
+    // Driving Parameters - Note that these are not the maximum capable speeds of
+    // the robot, rather the allowed maximum speeds
+    public static final double kMaxSpeedMetersPerSecond = 4.8;
+    public static final double kMaxAngularSpeed = 2 * Math.PI; // radians per second
+
+    // Chassis configuration
     public static final double kTrackWidth = Units.inchesToMeters(26.5);;
     // Distance between centers of right and left wheels on robot
     public static final double kWheelBase = Units.inchesToMeters(26.5);;
@@ -35,19 +41,13 @@ public final class Constants {
         new Translation2d(-kWheelBase / 2, kTrackWidth / 2),
         new Translation2d(-kWheelBase / 2, -kTrackWidth / 2));
 
-    public static final boolean kGyroReversed = false;
-
-    /* Driving Parameters */
-    public static final double kMaxSpeedMetersPerSecond = 4.8;
-    public static final double kMaxAngularSpeed = 2 * Math.PI; // radians per second
-
     // Angular offsets of the modules relative to the chassis in radians
     public static final double kFrontLeftChassisAngularOffset = -Math.PI / 2;
     public static final double kFrontRightChassisAngularOffset = 0;
     public static final double kBackLeftChassisAngularOffset = Math.PI;
     public static final double kBackRightChassisAngularOffset = Math.PI / 2;
 
-    /* SPARK MAX CAN IDs */
+    // SPARK MAX CAN IDs
     public static final int kFrontLeftDrivingCanId = 11;
     public static final int kRearLeftDrivingCanId = 13;
     public static final int kFrontRightDrivingCanId = 15;
@@ -57,16 +57,22 @@ public final class Constants {
     public static final int kRearLeftTurningCanId = 12;
     public static final int kFrontRightTurningCanId = 14;
     public static final int kRearRightTurningCanId = 16;
+
+    public static final boolean kGyroReversed = false;
   }
 
   public static final class ModuleConstants {
     public static final boolean kTurningEncoderInverted = true;
-    
-    public static final double kpinonTeeth = 14;  // adujust based off module ordered 
-    public static final double kmotorFreeSpeed =5676 /60;
-    public static final double kDrivingMotorReduction = 990 / (kpinonTeeth * 15);
+
+    // Calculations required for driving motor conversion factors and feed forward
+    public static final double kPinionTeeth = 14; // Adjust this to match your configuration!
+    public static final double kMotorFreeSpeed = 5676 / 60;
+    public static final double kDrivingMotorReduction = 990 / (kPinionTeeth * 15);
     public static final double kWheelDiameterMeters = 0.0762;
     public static final double kWheelCircumferenceMeters = kWheelDiameterMeters * Math.PI;
+    public static final double kDriveTrainFreeSpeed = (kMotorFreeSpeed * kWheelCircumferenceMeters)
+        / kDrivingMotorReduction; // calculated motor free speed
+
     public static final double kDrivingEncoderPositionFactor = (kWheelDiameterMeters * Math.PI)
         / (double) kDrivingMotorReduction; // meters
     public static final double kDrivingEncoderVelocityFactor = ((kWheelDiameterMeters * Math.PI)
@@ -77,12 +83,11 @@ public final class Constants {
 
     public static final double kTurningEncoderPositionPIDMinInput = 0; // radians
     public static final double kTurningEncoderPositionPIDMaxInput = kTurningEncoderPositionFactor; // radians
-    public static final double kdriveTrainFreeSpeed = (kmotorFreeSpeed * kWheelCircumferenceMeters)/kDrivingMotorReduction; //calculated motor free speed
 
     public static final double kDrivingP = 0.04;
     public static final double kDrivingI = 0;
     public static final double kDrivingD = 0;
-    public static final double kDrivingFF = 1/kdriveTrainFreeSpeed;
+    public static final double kDrivingFF = 1 / kDriveTrainFreeSpeed;
     public static final double kDrivingMinOutput = -1;
     public static final double kDrivingMaxOutput = 1;
 
