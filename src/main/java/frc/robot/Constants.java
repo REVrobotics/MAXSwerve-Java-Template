7554779f7dc969ae2d3bc457fac51be4e80763f4
Jan-Felix Abellera <jfabellera@gmail.com>
2024-01-08 19:55:18 -0600

    Update REVLib to 2024.2.0

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,7 +4,7 @@
 
 package frc.robot;
 
-import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkBase.IdleMode;
 
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
